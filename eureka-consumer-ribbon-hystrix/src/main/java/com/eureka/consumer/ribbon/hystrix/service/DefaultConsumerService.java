5d6eca28a623e4dd5695a76216f03fed63a3db28
alexpdh <alexpdh@163.com>
2018-01-17 11:42:20 +0800

    rebuild

---
+++ eureka-consumer-ribbon-hystrix/src/main/java/com/eureka/consumer/ribbon/hystrix/service/DefaultConsumerService.java
@@ -0,0 +1,28 @@
+package com.eureka.consumer.ribbon.hystrix.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 服务消费实现类
+ *
+ * @author pengdh
+ * @date 2018/01/17
+ */
+@Service
+public class DefaultConsumerService implements ConsumerService {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@HystrixCommand(fallbackMethod = "fallback")
+	public String consumer() {
+		return restTemplate.getForObject("http://eureka-client/dc", String.class);
+	}
+
+	public String fallback() {
+		return "fallback";
+	}
+}
